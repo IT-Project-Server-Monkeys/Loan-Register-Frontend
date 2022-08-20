3ef20131ba482dba7ba2ed30c5aee990acaa1820
Bruce Zhu <bruce@itrazotracetech.com>
2022-08-20 23:40:50 +1000

    initialise existing empty page files

---
+++ src/pages/ItemHistory.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ItemHistory = () => {
+  return <div>ItemHistory</div>;
+};
+
+export default ItemHistory;
---
+++ src/pages/LoaneeDashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoaneeDashboard = () => {
+  return <div>LoaneeDashboard</div>;
+};
+
+export default LoaneeDashboard;
