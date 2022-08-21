30efccd1e87991b4d2986744cdaaf560e8ba4960
Vivien Guo <guov@student.unimelb.edu.au>
2022-08-21 14:45:48 +1000

    code cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../styles/Header.module.scss"; // component scoped style
 import logo from "../images/logo.svg";
 
 const Header = (props) => {
+  useEffect(() => {
+    console.log(window.location.pathname);
+  }, []);
+
   const handleLogin = () => {
     props.onLogin({ userId: 1234 });
   };
+
+  const NavLink = (navProps) => {
+    return (
+      <a href={navProps.href} className={`${styles.a}`}>
+        <button className={`${styles.button}`} onClick={navProps.onClick}>{navProps.children}</button>
+      </a>
+    );
+  }
 
   const Nav = () => {
     if (props.loginSession != null) {
       return (
         <div className={`${styles.nav}`}>
-          <button className={`${styles.button}`}>
-            <a href="/dashboard/loaner" className={`${styles.a}`}>Dashboard</a>
-          </button>
-          <button className={`${styles.button}`}>
-            <a href="/account" className={`${styles.a}`}>Account</a>
-          </button>
-          <button className={`${styles.button}`} onClick={props.onLogout}>Log Out</button>
+          <NavLink href="/dashboard/loaner">Dashboard</NavLink>
+          <NavLink href="/account">Account</NavLink>
+          <NavLink href="/" onClick={props.onLogout}>Log Out</NavLink>
         </div>
       );
     } else {
       return (
         <div className={`${styles.nav}`}>
-          <button onClick={handleLogin} className={`${styles.button}`} >
-            <a href="/login" className={`${styles.a}`}>Log In</a>
-          </button>
+          <NavLink href="/login" onClick={handleLogin}>Log In</NavLink>
         </div>
       );
     }
@@ -33,7 +39,7 @@ const Header = (props) => {
 
   return (
     <div className={`${styles.header}`}>
-      <a href="/" style={{height: "fit-content"}}>
+      <a href="/" style={{margin: "0.5rem 0 0.5rem 0"}}>
         <img
           className={`${styles.logo}`}
           src={logo}
