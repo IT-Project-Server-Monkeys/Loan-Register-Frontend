295867073f58fa53462f816f8059313d2802a035
Bruce Zhu <bruce@itrazotracetech.com>
2022-08-24 18:13:13 +1000

    style dashboard navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <h1>asdsd</h1>
+    </div>
   );
 };
 
--- src/pages/LoanerDashboard.js
+++ src/pages/LoanerDashboard.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import { Row, Col, Form, Button } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
+import '../styles/Dashboard.scss'
+import { AiOutlineUnorderedList, AiFillPlusCircle, AiOutlineUserSwitch } from 'react-icons/ai'
+import { MdQueryStats } from 'react-icons/md'
 
 const LoanerDashboard = (props) => {
   return (
-    <div>
+    <div className='page-margin dashboard'>
       <Row>
-        <Col md='6'>Filter Area</Col>
-        <Col md='6'>Board</Col>
+        <Col md='3' className='bg-light-blue' style={{minHeight: '30rem'}}>
+          <div>
+            Filters
+          </div>
+        </Col>
+        <Col style={{marginLeft: '4rem'}}>
+          <Row className='bg-light-blue' style={{height: '5rem'}}>
+            <div className='dashboard-nav'>
+              <div style={{width: '40%', maxWidth: '25rem'}}>
+                <span className='icon-blue'>
+                  <AiOutlineUnorderedList size={30} />
+                </span>
+                <div>
+                  <input type="search" placeholder='Search for items' />
+                </div>
+                <span className='icon-plus' style={{borderRadius: '50%'}}>
+                  <AiFillPlusCircle size={45} color='#0073e6' />
+                </span>
+              </div>             
+              <div style={{width: '12%', maxWidth: '8rem'}}>
+                <span className='icon-blue'>
+                  <MdQueryStats size={30} />
+                </span>
+                <span className='icon-blue'>
+                  <AiOutlineUserSwitch size={30} />
+                </span>
+              </div>
+              
+            </div>
+          </Row>
+        </Col>
       </Row>
     </div>
   )
