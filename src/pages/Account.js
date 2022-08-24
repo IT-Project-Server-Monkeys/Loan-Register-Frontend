70118556f68bc5c3ab194d1bb3cf80e129d2f4ae
Vivien Guo <guov@student.unimelb.edu.au>
2022-08-24 22:25:57 +1000

    get request skeleton

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const Account = () => {
-  return <div>Account</div>;
+  const [userInfo, setUserInfo] = useState("aaaa");
+
+  // basic get request. TODO move to helper functions?
+  const fetchUser = async () => {
+    await axios.get("http://webcode.me")
+    // await axios.get("https://server-monkeys-backend-test.herokuapp.com/users", {params: {_id: "62fd8a9df04410afbc6df31f"}})
+      .then((res) => {setUserInfo(res.data);})
+      .catch((err) => {setUserInfo(JSON.stringify(err));})
+  }
+
+  useEffect(() => {
+    fetchUser();
+    console.log("fetch");
+  }, []);
+
+
+  return <div>{userInfo}</div>;
 };
 
 export default Account;
