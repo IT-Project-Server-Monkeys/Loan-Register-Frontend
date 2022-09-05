7078b1a66f47a8f03edb5c009176f264adf837ce
jtychng <80381558+jtychng@users.noreply.github.com>
2022-09-05 11:59:27 +1000

    created landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,36 @@
 import React from "react";
+import '../styles/LandingPage.scss';
 
 const Home = () => {
   return (
-    <div>
-      <h1>asdsd</h1>
+    <div className={"landing-page"}>
+      <div className={"img-one"}>
+      </div>
+      <div className={"intro-box"}>
+        <div className={"intro-text"}>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        </div>
+        <div className={"icon"}>
+        </div>
+      </div>
+      <div className={"flex-box"}>
+        <div className={"img-two"}>
+        </div>
+        <div className={"text-box"}>
+        </div>
+      </div>
+      <div className={"flex-box"}>
+        <div className={"text-box"}>
+          Ready to loan an item?
+          <div className={"text-btn-default"}>
+            Get Started
+          </div>
+        </div>
+        <div className={"img-three"}>
+        </div>
+      </div>
     </div>
+
   );
 };
 
