707a213ec77123c669728a14808f6eecb9710d0f
jtychng <80381558+jtychng@users.noreply.github.com>
2022-09-07 11:35:12 +1000

    minor fixes to login and signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from "../styles/Login.module.scss";
+import "../styles/Login.scss";
 import { TextBkgBox, TextButton } from '../components';
 
 const Login = (props) => {
@@ -13,28 +13,31 @@ const Login = (props) => {
   };
 
   return (
-    <div className={`${styles.background}`}>
-      <TextBkgBox>
-        <div className="h1">
-          Log in to LR!
-        </div>
-        <div className={"inline-flex"}>
-          <div className="h3">
-            Username:
+    <div className={"login"}>
+      <div className={"background"}>
+        <TextBkgBox>
+          <div className="h1">
+            Log in to LR!
           </div>
-          <input type="text" placeholder="Enter username" className={"input-box"}/>
-        </div>
-        <div className={"inline-flex"}>
-          <div className="h3">
-              Password:
+          <div className={"inline-flex"}>
+            <div className="h3">
+              Username:
+            </div>
+            <input type="text" placeholder="Enter username" className={"input-box"}/>
           </div>
-          <input type="text" placeholder="Enter password" className={"input-box"}/>
-        </div>
-        <a href="/signup" className="a">Forgot password?</a>
-        <a href="/signup" className="a">New user?</a>
-        <TextButton onClick={handleLogin} className={`${styles.button}`}>Login</TextButton>
-      </TextBkgBox>
+          <div className={"inline-flex"}>
+            <div className="h3">
+                Password:
+            </div>
+            <input type="text" placeholder="Enter password" className={"input-box"}/>
+          </div>
+          <a href="/signup" className="a">Forgot password?</a>
+          <a href="/signup" className="a">New user?</a>
+          <TextButton onClick={handleLogin} className={"button"}>Login</TextButton>
+        </TextBkgBox>
+      </div>
     </div>
+
   );
 };
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,35 +1,37 @@
 import React from 'react';
-import styles from "../styles/Signup.module.scss";
+import "../styles/Signup.scss";
 import { TextBkgBox, TextButton } from '../components';
 
 const Signup = () => {
   return (
-    <div className={`${styles.background}`}>
-      <TextBkgBox>
-        <div className="h1">
-          Sign up to LR!
-        </div>
-        <div className={"inline-flex"}>
-          <div className="h3">
-            Username:
+    <div className={"sign-up"}>
+      <div className={"background"}>
+        <TextBkgBox>
+          <div className="h1">
+            Sign up to LR!
           </div>
-          <input type="text" placeholder="Enter username" className={"input-box"}/>
-        </div>
-        <div className={"inline-flex"}>
-          <div className="h3">
-              Password:
+          <div className={"inline-flex"}>
+            <div className="h3">
+              Username:
+            </div>
+            <input type="text" placeholder="Enter username" className={"input-box"}/>
           </div>
-          <input type="text" placeholder="Enter password" className={"input-box"}/>
-        </div>
-        <div className={"inline-flex"}>
-          <div className={`${styles.wrap}`}>
-              Confirm password:
+          <div className={"inline-flex"}>
+            <div className="h3">
+                Password:
+            </div>
+            <input type="text" placeholder="Enter password" className={"input-box"}/>
           </div>
-          <input type="text" placeholder="Enter password" className={"input-box"}/>
-        </div>
-        <a href="/login" className="a">Existing user?</a>
-        <TextButton onClick="" className={`${styles.button}`}>Sign up</TextButton>
-      </TextBkgBox>
+          <div className={"inline-flex"}>
+            <div className={"wrap"}>
+                Confirm password:
+            </div>
+            <input type="text" placeholder="Enter password" className={"input-box"}/>
+          </div>
+          <a href="/login" className="a">Existing user?</a>
+          <TextButton onClick="" className={"button"}>Sign up</TextButton>
+        </TextBkgBox>
+      </div>
     </div>
   );
 };
