7e94280df8e470e210b9f1389b292b20f352088e
Vivien Guo <guov@student.unimelb.edu.au>
2022-10-11 01:19:06 +1100

    make loan submitter auto make items visible

--- src/pages/ItemDetails.js
+++ src/pages/ItemDetails.js
@@ -3,7 +3,7 @@ import { useParams, useNavigate/*, useLocation*/, Link } from "react-router-dom"
 import '../styles/ItemPage.scss'
 import { LoanForm, TextButton, Loading, Submitting, NoAccess } from '../components';
 import { MdEdit } from 'react-icons/md';
-import { fetchItem, makeVisible } from "../utils/itemHelpers";
+import { fetchItem } from "../utils/itemHelpers"; // i've made loan submit helper set visible true
 import { createLoan, editLoan, fetchAllUsernames, fetchCurLoan, returnLoan } from "../utils/loanHelpers";
 import { noAccessRedirect, toDDMMYYYY, noCaseCmp } from "../utils/helpers";
 import noImg from "../images/noImage_300x375.png";
@@ -86,13 +86,7 @@ const ItemDetails = (props) => {
     setSubmitting(true);
     await returnLoan(
       item,
-      () => {
-        if (!item.visible) {
-          // if item is hidden, make it visible
-          // makeVisible(item.item_id)
-        }
-        window.location.reload()
-      },
+      () => window.location.reload(), // i've made loan submit helper set visible true
       () => {
         setSubmitting(false);
         alert("There was an error saving your loan. Please try again later.");
